841d392c04dcfaf2521db44a487b2a1f2b07f90f
Md Jihanur Rahman <64974221+jihanurrahman33@users.noreply.github.com>
2025-02-05 23:46:53 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,21 @@ class App extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(title: Text('Add Employee')),
-      body: Column(
-        children: [
-          TextFormField(
-            decoration: InputDecoration(hintText: 'Name'),
-          ),
-          TextFormField(decoration: InputDecoration(hintText: 'Age')),
-          TextFormField(decoration: InputDecoration(hintText: 'Salary')),
-          ElevatedButton(onPressed: () {}, child: Text('Add Employee'))
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(25),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(hintText: 'Name'),
+            ),
+            SizedBox(height: 10),
+            TextFormField(decoration: InputDecoration(hintText: 'Age')),
+            SizedBox(height: 10),
+            TextFormField(decoration: InputDecoration(hintText: 'Salary')),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: () {}, child: Text('Add Employee'))
+          ],
+        ),
       ),
     ));
   }
